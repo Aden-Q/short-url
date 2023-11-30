ed082920619d398b15eb29432cf67303dea97948
Aden-Q <qian0102@umn.edu>
2023-11-30 11:55:33 -0600

    feat: add a redirect handler for the GET REST endpoint

--- internal/handler/redirect.go
+++ internal/handler/redirect.go
@@ -1,10 +1,13 @@
 package handler
 
 import (
+	"errors"
 	"net/http"
 
 	"github.com/Aden-Q/short-url/internal/db"
+	"github.com/Aden-Q/short-url/internal/model"
 	"github.com/gin-gonic/gin"
+	"gorm.io/gorm"
 )
 
 // Used for URI binding
@@ -15,7 +18,7 @@ type bindingURI struct {
 
 // ShortenHandlerFunc shortens a long URL
 func RedirectHandler(c *gin.Context) {
-	_, ok := c.MustGet("dbConn").(*db.Engine)
+	db, ok := c.MustGet("dbConn").(*db.Engine)
 	if !ok {
 		c.JSON(500, gin.H{"message": "dbConn not found"})
 		return
@@ -23,8 +26,11 @@ func RedirectHandler(c *gin.Context) {
 
 	var binding bindingURI
 	if err := c.ShouldBindUri(&binding); err != nil {
+		// TODO: add a log here
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+		return
 	} else {
+<<<<<<< HEAD
 		// Write the URL to the database
 		c.JSON(http.StatusOK, gin.H{"shortURL": binding.ShortURL})
 =======
@@ -43,5 +49,22 @@ func RedirectHandler(db *db.DBEngine) gin.HandlerFunc {
 			c.JSON(200, gin.H{"longURL": shortURL.shortURL})
 		}
 >>>>>>> f6ad432 (feat: add a handler pkg)
+=======
+		// if there's a record in the database, redirect to the long URL
+		// otherwise return 404
+		var url model.URL
+		if err := db.First(&url, "short_url = ?", binding.ShortURL).Error; err == nil {
+			c.Redirect(http.StatusMovedPermanently, url.LongURL)
+			return
+		} else if errors.Is(err, gorm.ErrRecordNotFound) {
+			c.JSON(http.StatusNotFound, gin.H{"message": "URL not found"})
+			return
+		} else {
+			// other errors on the server side
+			c.JSON(http.StatusInternalServerError, gin.H{"message": err.Error()})
+		}
+
+		return
+>>>>>>> 0a4130b (feat: add a redirect handler for the GET REST endpoint)
 	}
 }
