4faa8a9f2a4a8aacb143cf054f3bf9a67b2f8d7a
Aden-Q <qian0102@umn.edu>
2023-11-30 12:19:29 -0600

    feat!: implement a url shortener web server

--- internal/handler/redirect.go
+++ internal/handler/redirect.go
@@ -11,7 +11,6 @@ import (
 )
 
 // Used for URI binding
-<<<<<<< HEAD
 type bindingURI struct {
 	ShortURL string `uri:"shortURL" binding:"required"`
 }
@@ -30,26 +29,6 @@ func RedirectHandler(c *gin.Context) {
 		c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
 		return
 	} else {
-<<<<<<< HEAD
-		// Write the URL to the database
-		c.JSON(http.StatusOK, gin.H{"shortURL": binding.ShortURL})
-=======
-type shortURL struct {
-	shortURL string `uri:"shortURL" binding:"required"`
-}
-
-// ShortenHandlerFunc shortens a long URL
-func RedirectHandler(db *db.DBEngine) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var shortURL shortURL
-		if err := c.ShouldBindQuery(&shortURL); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		} else {
-			// Write the URL to the database
-			c.JSON(200, gin.H{"longURL": shortURL.shortURL})
-		}
->>>>>>> f6ad432 (feat: add a handler pkg)
-=======
 		// if there's a record in the database, redirect to the long URL
 		// otherwise return 404
 		var url model.URL
@@ -65,6 +44,5 @@ func RedirectHandler(db *db.DBEngine) gin.HandlerFunc {
 		}
 
 		return
->>>>>>> 0a4130b (feat: add a redirect handler for the GET REST endpoint)
 	}
 }
--- internal/handler/shorten.go
+++ internal/handler/shorten.go
@@ -1,7 +1,6 @@
 package handler
 
 import (
-<<<<<<< HEAD
 	"errors"
 	"net/http"
 
@@ -63,28 +62,5 @@ func ShortenHandler(c *gin.Context) {
 		}
 
 		return
-=======
-	"net/http"
-
-	"github.com/Aden-Q/short-url/internal/db"
-	"github.com/gin-gonic/gin"
-)
-
-// Used for query binding
-type longURL struct {
-	longURL string `form:"longURL" binding:"required"`
-}
-
-// ShortenHandler shortens a long URL
-func ShortenHandler(db *db.DBEngine) gin.HandlerFunc {
-	return func(c *gin.Context) {
-		var longURL longURL
-		if err := c.ShouldBindQuery(&longURL); err != nil {
-			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
-		} else {
-			// Write the URL to the database
-			c.JSON(200, gin.H{"longURL": longURL.longURL})
-		}
->>>>>>> f6ad432 (feat: add a handler pkg)
 	}
 }
